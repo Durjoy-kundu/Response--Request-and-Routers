e7e2dcd715d422b2e351427dd93f76b6ba477dda
Durjoy kundu <durjoykundu07@gmail.com>
2025-01-30 13:07:01 +0600

    response , request and Routers

---
+++ Agenda.md
@@ -0,0 +1,7 @@
+**Response, Request and Routers**
+
+1. handeling post and other requests
+2. chaining the request
+3. Serving html files
+4. installing postman
+5. Express Router
---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("hey it is a get request")
+  res.send('Hello World2!')
+})
+
+app.use(express.static("public"))
+
+app.post('/', (req, res) => {
+    console.log("hey it is a post request")
+  res.send('Hello World post!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+      
